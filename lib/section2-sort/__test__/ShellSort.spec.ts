a3412865fd9eaf9edfae2affbaf955f0a9f146d4
amnhh <2570079254@qq.com>
2020-08-30 10:39:48 +0800

    chore: Make array.lang.ts to be functional expressions.

--- lib/section2-sort/__test__/ShellSort.spec.ts
+++ lib/section2-sort/__test__/ShellSort.spec.ts
@@ -7,9 +7,16 @@ describe('Shell Sort cases', () => {
     const input1 = generateUnsortedArray(1000)
     const result1 = [...input1].sort((v1, v2) => v1 - v2)
 
+    const input2 = generateUnsortedArray(10)
+    const result2 = [...input2].sort((v1, v2) => v1 - v2)
+
     it('shell sort correctly', () => {
-        const shellSort = new ShellSort([...input1])
-        shellSort.show();
-        assert.deepEqual<number[]>(result1, shellSort.getOutput())
+        const shellSort1 = new ShellSort([...input1])
+        shellSort1.show();
+        assert.deepEqual<number[]>(result1, shellSort1.getOutput())
+
+        const shellSort2 = new ShellSort([...input2])
+        shellSort2.show();
+        assert.deepEqual<number[]>(result2, shellSort2.getOutput())
     })
 })
--- lib/utils/generator/array.lang.ts
+++ lib/utils/generator/array.lang.ts
@@ -1,9 +1,33 @@
 /**
+ * 获取空白数组
+ *
+ * @param size
+ */
+export const generateEmptyArray = (size: number): number[] => {
+    return [...Array<number>(size)]
+}
+
+/**
+ * 获取 base <= target <= 2 * base 的随机值
+ *
+ * @param base
+ */
+export const getRandomNumber = (base: number): number => (base + Math.floor(Math.random() * base))
+
+
+/**
+ * 获取随机的结果
+ *
+ * @param list
+ */
+export const getRandomResult = (): number => Math.random() - 0.5
+
+/**
  * 获取乱序数组
  * @param size
  */
 export const generateUnsortedArray = (size: number): number[] => {
-    return [...Array<number>(size)]
-        .map((v, i) => i + Math.floor(Math.random() * i))
-        .sort(() => Math.random() - 0.5)
+    return generateEmptyArray(size)
+        .map((v, i) => getRandomNumber(i))
+        .sort(getRandomResult)
 }
