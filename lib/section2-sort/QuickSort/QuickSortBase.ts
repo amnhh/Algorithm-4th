cfd82623cea93e83840b82c3f8af2aaeb940fc17
anning03 <anning03@meituan.com>
2020-09-01 10:38:36 +0800

    feat: add quick sort

---
+++ lib/section2-sort/QuickSort/QuickSortBase.ts
@@ -0,0 +1,11 @@
+import SortBase from '../SortBase'
+
+export default class QuickSortBase extends SortBase {
+    sort (list: number[]): number[] {
+        return this._sort(list);
+    }
+
+    _sort(list: number[]): number[] {
+        return list
+    }
+}
